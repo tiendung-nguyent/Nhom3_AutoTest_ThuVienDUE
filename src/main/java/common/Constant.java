297151ffa735ff775aec59f33c59d99bd3715613
tiendung-nguyent <nguyenthitiendung@gmail.com>
2026-04-21 11:28:43 +0700

    Initial commit

---
+++ src/main/java/common/Constant.java
@@ -0,0 +1,10 @@
+package common;
+import org.openqa.selenium.WebDriver;
+
+public class Constant {
+    public static WebDriver WEBDRIVER;
+
+    public static final String THUVIEN_URL = "http://127.0.0.1:8000/";
+    public static final String USERNAME = "dung";
+    public static String PASSWORD = "123";
+}
